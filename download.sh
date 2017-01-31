986a38033f064aacaaa08ad6849068899a9dc386
Derek Tracy <tracyde@gmail.com>
2017-01-31 12:57:42 -0500

    updated docs and binaries

--- download.sh
+++ download.sh
@@ -2,6 +2,8 @@
 
 DIR=$PWD
 
+rm -rf bin
+
 for platform in "debian32" "debian64" "macosx64" "redhat32" "redhat64" "windows32" "windows64"; do
  mkdir -p bin/$platform
  cd bin/$platform
@@ -18,5 +20,8 @@ for platform in "linux32-binary" "linux64-binary" "macosx64-binary" "windows32-e
 done
 
 cd $DIR
+rm -rf docs
+mkdir -p docs
 cd docs
 wget --content-disposition "http://docs.pivotal.io/archives/pcf-docs-1.7.pdf"
+wget --content-disposition "http://docs.pivotal.io/archives/pcf-docs-1.9.pdf"
