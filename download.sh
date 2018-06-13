84476a8ad54148b40c5c01616d32ca024709c8dc
Derek Tracy <tracyde@gmail.com>
2018-06-13 17:59:02 -0400

    [#158271694] updated to new cf-cli releases and updated domain

--- download.sh
+++ download.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+VERSION=6.36.2
+DOCS_VERSION=2.0
 DIR=$PWD
 
 rm -rf bin
@@ -7,7 +9,7 @@ rm -rf bin
 for platform in "debian32" "debian64" "macosx64" "redhat32" "redhat64" "windows32" "windows64"; do
  mkdir -p bin/$platform
  cd bin/$platform
- wget --content-disposition "https://cli.run.pivotal.io/stable?release=${platform}&version=6.18.0&source=github-rel"
+ wget --content-disposition "https://cli.run.pivotal.io/stable?release=${platform}&version=${VERSION}&source=github-rel"
  cd $DIR
 done
 
@@ -15,13 +17,13 @@ cd $DIR
 for platform in "linux32-binary" "linux64-binary" "macosx64-binary" "windows32-exe" "windows64-exe"; do
   mkdir -p bin/$platform
   cd bin/$platform
-  wget --content-disposition "https://cli.run.pivotal.io/stable?release=${platform}&version=6.18.0&source=github-rel"
+  wget --content-disposition "https://cli.run.pivotal.io/stable?release=${platform}&version=${VERSION}&source=github-rel"
   cd $DIR
 done
 
 cd $DIR
 rm -rf docs
 mkdir -p docs
+
 cd docs
-wget --content-disposition "http://docs.pivotal.io/archives/pcf-docs-1.7.pdf"
-wget --content-disposition "http://docs.pivotal.io/archives/pcf-docs-1.9.pdf"
+wget --content-disposition "http://docs.pivotal.io/archives/pcf-docs-${DOCS_VERSION}.pdf"
